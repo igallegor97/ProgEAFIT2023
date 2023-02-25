932e6d760a242199e0cd383029dbada01a78c5f4
Isa Gallego <88630062+igallegor97@users.noreply.github.com>
2023-02-25 18:45:52 -0300

    Task1&2.py
    Versión final.

--- Task1.py
+++ Task1.py
@@ -1,32 +1,57 @@
-# Importar módulos.
-
+# Importar módulos.  
+import pandas as pd 
 import sys
 
-# Definir la función de búsqueda.
+# Definir los argumentos.
+FIELDS = sys.argv[1].split(',')
+FROM = sys.argv[2]()
+WHERE = sys.argv[3].split('#')[0]
+VALUE = sys.argv[3].split('#')[1]
 
-def busqueda(file, string, query, fields):
-  hits=0
-  hits_analizados=0
-  record=[]
-  
-  while True:
-    line=file.readline()  
-    if line.startswith("ID"):
-      hits_analizados +=1
-      record.clear()
-      record=[line[:-1]]
-    elif line.startswith("//"):
-      
-      for i in record:   
-        if string in i.startswith(query):
-          hits += 1     
+# Función principal:
+
+def funcion_principal(FROM, WHERE, VALUE, FIELDS):
+    records = []  # Crear una lista vacía para guardar los registros de la base de datos 
+    with open(FROM) as f:
+        record = {}  # Crear un diccionario vacío para almacenar los campos de los registros
+        for line in f:
+            line = line.strip()
+            if line.startswith('ID'):
+                if record:  # Iteración para agregar los registros a la lista 
+                    records.append(record)
+                record = {'id': line.split()[1]}  # Empezar un registro nuevo
+            elif line.startswith('//'):
+                if record:  # Agregar el último registro a la lista
+                    records.append(record)
+                record = {}  
+            else:
+                field = line[:2].strip()  # Extraer el campo (FIELD) 
+                value = line[5:].strip()  # Extraer los valores del campo 
+                if field in record:
+                    record[field] += '\n' + value  # Concatenar múltiples líneas del mismo campo
+                else:
+                    record[field] = value  # Agregar un nuevo campo
+
+    # Convertir la lista de diccionarios creados en un DataFrame de pandas.
+    df = pd.DataFrame.from_records(records)
+
+    # Filtrar los datos por la condición WHERE
+    full_record = df[df[WHERE] == VALUE]
+
+    # Agregar al DataFrame una nueva columna con el porcentaje del total de registros que coinciden con el query
+    total_records = len(df)
+    filtered_records = len(full_record)
+    full_record['Percentage'] = round((filtered_records/total_records)*100, 2)
+
+    # Seleccionar los FIELDS deseados
+    filtered_record = full_record[FIELDS]
+
+    # Dirigir los resultados a un archivo .fasta en caso de que el campo 'SQ' sea pedido en el query
+    if 'sq' in FIELDS:
+        with open('result.fasta', 'w') as f:
+            for index, row in filtered_record.iterrows():
+                f.write('>{}\n{}\n'.format(row['id'], row['sq']))
     else:
-      record.append(line[:-1])
-    return hits_analizados, hits
+        print(filtered_record)
 
-hits_analizados, hits = busqueda(file, string, query, fields)
-file.close()
-
-file = open(uniprot_sprot.dat)
-print("Hits obtenidos", hits)
-print("Hits analizados ", hits_analizados)
+funcion_principal(FROM, WHERE, VALUE, FIELDS)
